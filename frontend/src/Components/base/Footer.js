b4c983f48aab1a65df0cf3a0fb08af1f2a12c2df
ivan.gorbushko <ivan.gorbushko@gmail.com>
2019-12-16 11:20:09 +0200

    Fixed footer

--- frontend/src/Components/base/Footer.js
+++ frontend/src/Components/base/Footer.js
@@ -16,17 +16,14 @@ class Footer extends Component {
                 {/*Footer Social Icons*/}
                 <div className="col-lg-4 mb-5 mb-lg-0">
                     <h4 className="text-uppercase mb-4">Around the Web</h4>
-                    <a className="btn btn-outline-light btn-social mx-1" href="#">
+                    <a className="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com/profile.php?id=100010531895443">
                         <i className="fab fa-fw fa-facebook-f"></i>
                     </a>
-                    <a className="btn btn-outline-light btn-social mx-1" href="#">
+                    <a className="btn btn-outline-light btn-social mx-1" href="https://twitter.com/IvanGorbushko">
                         <i className="fab fa-fw fa-twitter"></i>
                     </a>
-                    <a className="btn btn-outline-light btn-social mx-1" href="#">
+                    <a className="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com/">
                         <i className="fab fa-fw fa-linkedin-in"></i>
-                    </a>
-                    <a className="btn btn-outline-light btn-social mx-1" href="#">
-                        <i className="fab fa-fw fa-dribbble"></i>
                     </a>
                 </div>
                 {/*Footer About Text*/}
@@ -35,7 +32,6 @@ class Footer extends Component {
                     <p className="lead mb-0">Freelance is a free to use, MIT licensed Bootstrap theme created by
                         <a href="http://startbootstrap.com">Start Bootstrap</a>.</p>
                 </div>
-
             </div>
         </div>
     );
